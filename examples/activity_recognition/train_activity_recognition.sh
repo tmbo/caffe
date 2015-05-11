5d164c150160f4c3ac587e683753870f6bf58608
Stefan Bunk <stefan.bunk@googlemail.com>
2015-05-11 22:12:24 +0200

    Update shell script.

--- examples/activity_recognition/train_activity_recognition.sh
+++ examples/activity_recognition/train_activity_recognition.sh
@@ -1,5 +1,12 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-#../../build/tools/caffe train -solver solver.prototxt 2> out
-../../build/tools/caffe train -solver solver.prototxt 2>&1 | less
+export CAFFE_ROOT="~/caffe-tmbo"
+
+WEIGHTS=\
+$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
+
+$CAFFE_ROOT/build/tools/caffe train \
+    -solver $CAFFE_ROOT/examples/activity_recognition/activity_recognition_solver.prototxt \
+    -weights $WEIGHTS \
+    -gpu 0 2>&1 | less
 
