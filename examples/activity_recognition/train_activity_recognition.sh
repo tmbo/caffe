2114ddf508cade0d3179a76e2f62a10ac5e2fa2c
Stefan Bunk <stefan.bunk@googlemail.com>
2015-05-11 22:24:38 +0200

    Use /home/knub instead of ~.

--- examples/activity_recognition/train_activity_recognition.sh
+++ examples/activity_recognition/train_activity_recognition.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
-export CAFFE_ROOT="~/caffe-tmbo"
+export CAFFE_ROOT="$HOME/caffe-tmbo"
 
-WEIGHTS=\
-$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
+WEIGHTS=$CAFFE_ROOT/models/bvlc_reference_caffenet/bvlc_reference_caffenet.caffemodel
 
 $CAFFE_ROOT/build/tools/caffe train \
-    -solver $CAFFE_ROOT/examples/activity_recognition/activity_recognition_solver.prototxt \
+    -solver $CAFFE_ROOT/examples/activity_recognition/solver.prototxt \
     -weights $WEIGHTS \
     -gpu 0 2>&1 | less
 
